c438f1bd67600108b39436a8a96ba9ea15bdf963
teban192 <mmonestelalf@gmail.com>
2025-02-13 16:00:28 -0600

    Practice Python 13022025

--- firstProgram.py
+++ firstProgram.py
@@ -1,2 +1,22 @@
 mensaje = "Hola alumnos"
 print(mensaje)
+
+def mensaje():
+    print("Estamos aprendiendo Python")
+    print("Estamos aprendiendo instrucciones basicas")
+    print("Poco a poco iremos avanzando")
+
+
+def suma():
+    num1 =5
+    num2 =7
+    print(num1 + num2)
+
+def sumaParametros(num1,num2):
+    print(num1+num2)
+
+sumaParametros(5,7)
+sumaParametros(2,3)
+sumaParametros(35,358)
+    
+
